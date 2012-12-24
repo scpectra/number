4fe5d0aa841db7cd4da07d794c809d0d27202fad
Scpectra Dev <dev@scpectra.com>
2012-12-24 08:35:23 -0500

    Initial commit

---
+++ lib/src/complex.dart
@@ -0,0 +1,8 @@
+part of number;
+
+class Complex extends Number {
+  
+  Imaginary _imaginary;
+  
+  
+}
---
+++ lib/src/integer.dart
@@ -0,0 +1,8 @@
+part of number;
+
+class Integer extends Number {
+
+  Integer(int value) {
+    _value = value;
+  }
+}
